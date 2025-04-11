3a888a945708b94dd2a504cfd782c78f10003af5
Mateus Devino <mateus@mdevino.com>
2025-04-11 20:49:50 -0300

    Add app structure
    Signed-off-by: Mateus Devino <mateus@mdevino.com>

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,19 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    rsx! { "HotDog!" }
+    rsx! {
+        div {
+            id: "title",
+            h1 { "HotDog! 🌭" }
+        }
+        div {
+            id: "dogview",
+            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+        }
+        div {
+            id: "buttons",
+            button { id: "skip", "skip" }
+            button { id: "save", "save!" }
+        }
+    }
 }
